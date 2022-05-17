a93e144ef76433d1746ec8f36e8101862e0299aa
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-17 17:58:18 -0400

    Added components for form field that is reusable across portals

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,18 +1,47 @@
 import './LoginForm.scss';
 import React, { Component } from 'react';
+import axios from 'axios';
+import PortalBackground from "../../components/PortalBackground/PortalBackground";
+import Input from '../Input/Input';
 
 
 /* Used for both Client & Pilot Portal Logins */
 class LoginForm extends Component {
     state = {
+        error: "",
         success: false
     }
 
+    // handleSubmit = (event) => {
+    //     event.preventDefault();
+
+    //     axios
+    //         .post('http://localhost:8080/api/users/login', {
+    //             email: event.target.email.value,
+    //             password: event.target.password.value
+    //         })
+    //         .then((response) => {
+    //             sessionStorage.setItem("token", response.data.token);
+    //             this.setState({ success: true });
+    //         })
+    //         .catch((error) => {
+    //             this.setState({ error: error.response.data });
+    //         });
+    // };
+
     render() {
         return (
-            <form className="loginForm" onSubmit={this.handleSubmit}>
-                
-            </form>
+            <div className='loginForm'>
+                <PortalBackground />
+                <section className='loginForm-wrapper'>
+                    <form className="loginForm__form" onSubmit={this.handleLogin}>
+                        <h2 className="loginForm__title">Sign-in</h2>
+                        <Input type="text" name="email" label="Email" />
+                        <Input type="password" name="password" label="Password" />
+                        <button className='loginForm__btn'>Sign-In</button>
+                    </form>
+                </section>
+            </div>
         );
     }
 }
--- src/pages/Portal/Portal.js
+++ src/pages/Portal/Portal.js
@@ -3,16 +3,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import pilot from '../../assets/icons/pilot_icon.svg';
 import client from '../../assets/icons/client_icon.svg';
-import background from '../../assets/images/login-background.mp4';
+import PortalBackground from "../../components/PortalBackground/PortalBackground";
 
 export default function Portal() {
     return (
         <main className="login">
-            <div className="video-wrapper">
-                <video className="video" playsInline autoPlay="autoplay" muted loop={true}>
-                    <source src={background} type="video/mp4" />
-                </video>
-            </div>
+            <PortalBackground />
             <div className="login-wrapper">
                 <Link to='/portal/pilot' className="login__link">
                     <article className="login__card">
