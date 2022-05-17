f8c3d33ef469782bb8e3d72d4b557320bb597ad1
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-16 23:12:38 -0400

    Added hover effect to contrast colors on cards, & background for the page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,11 +2,16 @@ import "./Login.scss";
 import React from 'react';
 import pilot from '../../assets/icons/pilot_icon.svg';
 import client from '../../assets/icons/client_icon.svg';
-
+import background from '../../assets/images/login-background.mp4';
 
 function Login() {
     return (
         <main className="login">
+            <div className="video-wrapper">
+                <video className="video" playsInline autoplay="autoplay" muted loop="true">
+                    <source src={background} type="video/mp4" />
+                </video>
+            </div>
             <div className="login-wrapper">
                 <article className="login__card">
                     <h2 className="login__title">Pilot</h2>
