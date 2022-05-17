aac03fc3dedc10aadc5feaa6a003e1b8b08e3f88
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-17 18:10:40 -0400

    Styled input components for form

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 export default function Input({ label, name, type }) {
     return (
-            <label className='field__label'>
-                {/* {label} */}
-                <input placeholder={label} type={type} id={name} className="field__input" />
-            </label>
+            <div className='field'>
+                <label className='field__label'>{label}</label>
+                <input placeholder={label} type={type} id={name} className="field__input" /> 
+            </div>
     );
 }
 
