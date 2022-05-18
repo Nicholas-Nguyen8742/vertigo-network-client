93c41f4ad2e734248183f64c5c520bccac9083ae
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-18 16:54:47 -0400

    Added basic structure for sign-up form

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,24 +1,31 @@
 import './SignUp.scss';
 import React from 'react';
+import PortalBackground from '../../components/PortalBackground/PortalBackground';
+import Input from '../../components/Input/Input';
+import video from '../../assets/videos/portal_login.mp4';
 
 function SignUp() {
     return (
-        <section>
-            <form> 
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-            </form>
-        </section>
+        <main className='signUp-page'>
+            <PortalBackground video={video}/>
+            <section className='signUp'>
+                <form className='signUp__form' > 
+                    <h2 className='signUp__title'>Sign-Up</h2>
+                    <div className='signUp__form-left'>
+                        <Input />
+                        <Input />
+                        <Input />
+                        <Input />
+                    </div>
+                    <div className='signUp__form-right'> 
+                        <Input />
+                        <Input />
+                        <Input />
+                        <Input />
+                    </div>
+                </form>
+            </section>
+        </main>
     );
 }
 
