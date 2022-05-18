b605ed652cf11df2c65ade19a71b11ee4911ab12
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-18 17:17:30 -0400

    Added signup form's specification of pilot or client through radio button selection to html

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -4,7 +4,7 @@ import PortalBackground from '../../components/PortalBackground/PortalBackground
 import Input from '../../components/Input/Input';
 import video from '../../assets/videos/portal_login.mp4';
 
-function SignUp() {
+export default function SignUp() {
     return (
         <main className='signUp-page'>
             <PortalBackground video={video}/>
@@ -12,16 +12,25 @@ function SignUp() {
                 <form className='signUp__form' > 
                     <h2 className='signUp__title'>Sign-Up</h2>
                     <div className='signUp__form-left'>
-                        <Input />
-                        <Input />
-                        <Input />
-                        <Input />
+                        <Input type="text" name="firstName" label="First Name" />
+                        <Input type="text" name="lastName" label="Last Name"/>
+                        <Input type="email" name="email" label="Email"/>
+                        <div className='signUp__form-radio'>
+                            <label className='signUp__form-radio__label'>
+                                Pilot
+                                <input className='signUp__form-radio__input' type="radio" name="user" label="pilot"/>
+                            </label>
+                            <label className='signUp__form-radio__label'>
+                                Client
+                                <input className='signUp__form-radio__input'type="radio" name="user" value="client"/>
+                            </label>
+                        </div>
                     </div>
                     <div className='signUp__form-right'> 
-                        <Input />
-                        <Input />
-                        <Input />
-                        <Input />
+                        <Input type="text" name="city" label="City"/>
+                        <Input type="text" name="state" label="State"/>
+                        <Input type="email" name="password" label="Password"/>
+                        <Input type="password" name="confirmPassword" label="Confirm Password"/>
                     </div>
                 </form>
             </section>
@@ -29,4 +38,3 @@ function SignUp() {
     );
 }
 
-export default SignUp;
