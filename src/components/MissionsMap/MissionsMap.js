01edb76b070cc026becf787dc45ff5f653948785
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-23 01:26:13 -0400

    React Leaflet map for pilot missions is showing

--- src/components/MissionsMap/MissionsMap.js
+++ src/components/MissionsMap/MissionsMap.js
@@ -1,11 +1,36 @@
 import './MissionsMap.scss';
 import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
 
 export default function MissionsMap() {
     return (
-        <div>
-            
-        </div>
+        <section className='missionsMap'>
+            <MapContainer 
+                center={[28.5384, -81.3789]} 
+                zoom={11} 
+                scrollWheelZoom={true}>
+                <TileLayer
+                    attribution={'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
+                    url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+                />
+                {/* <Marker position={[33.5290172, -85.253076]}></Marker>
+                {playgrounds.map(playground => (
+                    <Marker 
+                    key={playground.id} 
+                    position={[playground.latitude, playground.longitude]}>
+                    <Popup position={[playground.longitude, playground.latitude]}>
+                        <div>
+                        <h2>{"Name: " + playground.name}</h2>
+                        <p>{"Address: " + playground.address} {playground.city + ", " + playground.state }</p>
+                        <p>{playground.city + " " + playground.state }</p>
+                        </div>
+                    </Popup>
+                    </Marker>
+                ))} */}
+            </MapContainer>
+        </section>
+
     );
 }
 
