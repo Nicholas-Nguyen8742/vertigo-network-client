4c08f911445feec6c8b43be2fe482cbf0f990278
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-22 20:05:51 -0400

    Added new icon assets and made dashboard's navbar

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,5 +1,12 @@
 import './DashboardPage.scss';
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import profileImg from '../../assets/images/IMG_8313.JPG';
+import home from '../../assets/icons/home_icon.png';
+import profile from '../../assets/icons/profile_icon.png';
+import missions from '../../assets/icons/drone_icon.png';
+import logout from '../../assets/icons/logout_icon.png';
+
 
 class DashboardPage extends Component {
     state = {
@@ -9,6 +16,36 @@ class DashboardPage extends Component {
     render() {
         return (
             <main className='dashboard-page'>
+                <nav className='navbar'>
+                    <img className='navbar__profile' src={profileImg} alt=""></img>
+                    <p className='navbar__user'>Mike Lopez</p>
+                    <ul className='navbar__list'>
+                        <NavLink 
+                            exact={true} 
+                            activeClassName="navbar__item-active" 
+                            className="navbar__item-inactive" 
+                            to='/dashboard'>
+                            <img className="navbar__icon-home" src={home} alt=''/>
+                            Dashboard</NavLink>
+                        <NavLink 
+                            activeClassName="navbar__item-active" 
+                            className="navbar__item-inactive" 
+                            to='/profile'>
+                            <img className="navbar__icon-profile" src={profile} alt=''/>
+                            Profile</NavLink> 
+                        <NavLink 
+                            activeClassName="navbar__item-active" 
+                            className="navbar__item-inactive" 
+                            to='/profile'>
+                            <img className="navbar__icon-drone" src={missions} alt=''/>
+                            Profile</NavLink> 
+                    </ul>
+                    <div className='navbar__logout'>
+                        <img className="navbar__icon" src={logout} alt=''/>
+                        <p>Logout</p>
+                    </div>
+                </nav>
+
 
             </main>
         );
