352c3397a8ae8848d861bf2bb25a114a4bba4076
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-22 23:51:10 -0400

    Turned Mission cards into component reusable for other pages

--- src/components/PendingMissions/PendingMissions.js
+++ src/components/PendingMissions/PendingMissions.js
@@ -1,22 +1,14 @@
 import './PendingMissions.scss';
 import React from 'react';
-import profile from '../../assets/images/IMG_8313.JPG';
+import MissionCard from '../MissionCard/MissionCard';
 
-function PendingMissions() {
+export default function PendingMissions() {
     return (
         <section className='pendingMissions'>
             <h2 className='pendingMissions__title'>Pending Missions</h2>
-            <article className='missionCard'>
-                <img className='missionCard__img' src={profile} alt=''/>
-                <div className='missionCard-right'>
-                    <h4  className='missionCard__title'>Colton Summers</h4>
-                    <h5 className='missionCard__specialty'>Real Estate</h5>
-                    <p className='missionCard__body1'>Aerial Photography</p>
-                    <h5 className='missionCard__location'>Kissimmee, FL</h5>
-                </div>
-            </article>
+            <MissionCard />
+            <MissionCard />
         </section>
     );
 }
 
-export default PendingMissions;
