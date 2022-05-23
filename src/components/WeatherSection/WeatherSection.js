3111a00b31fd37f575196333b714836ec60a0fd8
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-22 23:28:52 -0400

    Weather section and card styling done for desktop

--- src/components/WeatherSection/WeatherSection.js
+++ src/components/WeatherSection/WeatherSection.js
@@ -8,6 +8,8 @@ function WeatherSection() {
             <h2 className='weather__title'>Weather</h2>
             <article className='weather__card'>
                 <div className='weather-top'>
+                    <img className='weather__icon-top' src={sunny} alt='' />
+                    <span className='weather__temp-top'>86&deg;</span>
                     <h2 className='weather__location'>Orlando, FL</h2>
                 </div>
                 <div className='weather-bottom'>
