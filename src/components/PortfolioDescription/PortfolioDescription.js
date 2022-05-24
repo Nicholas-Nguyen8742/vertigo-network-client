77f30a412b23d92ba66ae19a1a26dec1b0d96c87
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-24 16:22:07 -0400

    Added profile description section to profile page

---
+++ src/components/PortfolioDescription/PortfolioDescription.js
@@ -0,0 +1,21 @@
+import './PortfolioDescription.scss';
+import React, { Component } from 'react';
+import profile from "../../assets/images/IMG_8313.JPG"
+
+export default class PortfolioDescription extends Component {
+    render() {
+        return (
+            <section className='portfolioDescription'>
+                <div className='portfolioDescription-top'>
+
+                </div>
+                <div className='portfolioDescription-bottom'>
+                    <img className='portfolioDescription__img' alt="" src={profile} />
+                    <button className='portfolioDescription__btn'>Lorem ipsum</button>
+                    <p className='portfolioDescription__body'>Orlando Florida native who loves to explore nature, rock climb, and meet new people. I have been a drone photographer and videographer for over 10 years, with experience in weddings, real estate, commercial and private sectors.</p>
+                </div>
+            </section>
+        );
+    }
+}
+
--- src/pages/ProfileReviewsPage/ProfileReviewsPage.js
+++ src/pages/ProfileReviewsPage/ProfileReviewsPage.js
@@ -2,13 +2,19 @@ import './ProfileReviewsPage.scss';
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import ProfileNav from '../../components/ProfileNav/ProfileNav';
+import PortfolioDescription from '../../components/PortfolioDescription/PortfolioDescription';
 
 export default function ProfileReviewsPage() {
     return (
-        <main className='reviews-page'>
+        <main className='profile-page'>
             <NavBar />
-            <div className='reviews-main'>
-                <ProfileNav btn="Reviews"/>
+            <div className='profile-main'>
+                <PortfolioDescription />
+                <ProfileNav btn="Reviews" />
+                <div className='profile-portfolio'>
+
+
+                </div>
             </div>
         </main>
     );
