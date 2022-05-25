78f3cd4ea4b82ddb849c651b61494c02c0a23c88
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-25 01:32:08 -0400

    Database connection through axios to server works good on portfolio profile reviews page

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -2,23 +2,21 @@ import './ReviewCard.scss';
 import React from 'react';
 import quote from '../../assets/icons/quote_icon.png';
 import { ReviewCardStars } from '../ReviewCardStars/ReviewCardStars';
-import profile from '../../assets/images/IMG_8313.JPG';
 
-
-export default function ReviewCard() {
+export default function ReviewCard({ key, firstName, lastName, profile, city, state, description, rating }) {
     return (
         <article className='reviewCard'>
             <div className='reviewCard-top'>
                 <img src={quote} className='reviewCard__quote' alt='' />
-                <ReviewCardStars key={1} rating={5} />
+                <ReviewCardStars key={key} rating={rating} />
             </div>
             <div className='reviewCard-middle'>
-                <p className='reviewCard__body'>The best drone photographer ever! My listings increased in views because of him</p>
+                <p className='reviewCard__body'>{description}</p>
             </div>
             <div className='reviewCard-bottom'>
                 <img className='reviewCard__img' alt="" src={profile} />
-                <h3 className='reviewCard__author'>Jacob Collins</h3>
-                <p className='reviewCard__company'>Keller Williams Reality</p>
+                <h3 className='reviewCard__author'>{firstName} {lastName}</h3>
+                <p className='reviewCard__company'>{city}, {state}</p>
             </div>
         </article>
     );
--- src/pages/ProfileReviewsPage/ProfileReviewsPage.js
+++ src/pages/ProfileReviewsPage/ProfileReviewsPage.js
@@ -1,34 +1,52 @@
 import './ProfileReviewsPage.scss';
 import React, { Component } from 'react';
+import { API_URL } from '../../utils/API';
+import axios from 'axios';
 import NavBar from '../../components/NavBar/NavBar';
 import ProfileNav from '../../components/ProfileNav/ProfileNav';
 import PortfolioDescription from '../../components/PortfolioDescription/PortfolioDescription';
 import ReviewCard from '../../components/ReviewCard/ReviewCard';
 
 export default class ProfileReviewsPage extends Component {
+    state = {
+        reviews: []
+    }
 
-render(){
-    const id = this.props.match.params.id;
+    componentDidMount() {
+        axios.get(`${API_URL}/pilots/${this.props.match.params.id}/reviews`)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    reviews: res.data
+                })
+            })
+    }
 
-    return (
-        <main className='profileReviews-page'>
-            <NavBar id={id}/>
-            <div className='profileReviews-main'>
-                <PortfolioDescription />
-                <ProfileNav id={id} btn="Reviews" />
-                <div className='profileReviews-portfolio'>
-                    <ReviewCard />
-                    <ReviewCard />
-                    <ReviewCard />
-                    <ReviewCard />
-                    <ReviewCard />
-                    <ReviewCard />
-                    <ReviewCard />
-
-
+    render() {
+        const id = this.props.match.params.id;
+        const { reviews } = this.state;
+        return (
+            <main className='profileReviews-page'>
+                <NavBar id={id} />
+                <div className='profileReviews-main'>
+                    <PortfolioDescription />
+                    <ProfileNav id={id} btn="Reviews" />
+                    <div className='profileReviews-portfolio'>
+                        {reviews.map((item) => (
+                            <ReviewCard
+                                key={item.timestamp}
+                                firstName={item.firstName}
+                                lastName={item.lastName}
+                                profile={item.profile}
+                                city={item.city}
+                                state={item.state}
+                                description={item.description} 
+                                rating={item.rating}
+                                />
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </main>
-    );
-}
+            </main>
+        );
+    }
 }
