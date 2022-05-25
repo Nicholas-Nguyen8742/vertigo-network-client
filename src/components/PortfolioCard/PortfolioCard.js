25f16c70eed64703c39e8abb9d96e9ef66d2813e
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-25 11:17:11 -0400

    Added small changes to PortfolioCard styling and html sstructure

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -1,15 +1,16 @@
 import './PortfolioCard.scss';
 import React from 'react';
 
-export default function PortfolioCard() {
+export default function PortfolioCard({ img, city, state, date, client }) {
     return (
         <article className='portfolioCard'>
-            <div className='portfolioCard-top'>
+            <div className='portfolioCard-top' style={{backgroundImage:`url(${img})`}}>
+                {/* <img className='portfolioCard__img' src={img} alt={`Images for ${client}`}/> */}
             </div>
             <div className='portfolioCard-bottom'>
-                <h4 className='portfolioCard__title'>National Geographic</h4>
-                <p className='portfolioCard__date'>Completed on 4/22/22</p>
-                <p className='portfolioCard__location'>San Diego, CA</p>
+                <h4 className='portfolioCard__title'>{client}</h4>
+                <p className='portfolioCard__date'>Completed on {date}</p>
+                <p className='portfolioCard__location'>{city}, {state}</p>
             </div>
         </article>
     );
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,5 +1,7 @@
 import './ProfilePage.scss';
 import React, { Component } from 'react';
+import { API_URL } from '../../utils/API';
+import axios from 'axios';
 import NavBar from '../../components/NavBar/NavBar';
 import ProfileNav from '../../components/ProfileNav/ProfileNav';
 import PortfolioDescription from '../../components/PortfolioDescription/PortfolioDescription';
@@ -7,35 +9,39 @@ import PortfolioCard from '../../components/PortfolioCard/PortfolioCard';
 
 export default class ProfilePage extends Component {
     state = {
-
+        portfolio: []
     }
+
+    componentDidMount() {
+        axios.get(`${API_URL}/pilots/${this.props.match.params.id}/portfolio`)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    portfolio: res.data
+                })
+            })
+    }
+
     render() {
         const id = this.props.match.params.id;
+        const { portfolio } = this.state;
         return (
             <main className='profile-page'>
-                <NavBar id={id}/>
+                <NavBar id={id} />
                 <div className='profile-main'>
                     <PortfolioDescription />
                     <ProfileNav id={id} btn="Portfolio" />
                     <div className='profile-portfolio'>
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
-                        <PortfolioCard />
+                        {portfolio.map((item) => (
+                            <PortfolioCard
+                                key={item.timestamp}
+                                img={item.img}
+                                city={item.city}
+                                state={item.state}
+                                date={item.dateCompleted}
+                                client={item.client}
+                            />
+                        ))}
                     </div>
                 </div>
             </main>
